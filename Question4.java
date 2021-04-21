c5a9069a5a02d71db86e4842f512b35a0f9c8163
elginloh <>
2021-04-21 12:42:57 +0000

    Question 4 done

--- Question4.java
+++ Question4.java
@@ -18,6 +18,16 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter a number: ");
+    int num = in.nextInt();
+    for(int i = 0; i <= num; i++)
+    {
+      for(int y = 0; y < i; y++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
   }
 }
